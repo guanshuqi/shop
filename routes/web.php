b1bf4de1aac69bb82d26b4266732e268004deb8f
guanshuqi <17710293967@163.com>
2019-01-15 20:13:26 +0800

    cdd

--- routes/web.php
+++ routes/web.php
@@ -74,7 +74,6 @@ Route::get('/pay','Pay\AliPayController@test');
 //支付宝调回
 
 Route::get('/orderPay/{oid}','Pay\AliPayController@pay')->middleware('check.login.token');
-Route::get('/orderPay/{order_id}','Pay\AliPayController@pay')->middleware('check.login.token');
 Route::post('/pay/alipay/notify','Pay\AliPayController@aliNotify');//异步通知
 Route::get('/pay/alipay/return','Pay\AliPayController@aliReturn');//同步通知
 
