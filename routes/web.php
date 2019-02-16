368389bb0ef9ecca1b1a5ba9638665a5839e31a3
guanshuqi <17710293967@163.com>
2019-02-16 15:58:33 +0800

    weixin11

--- routes/web.php
+++ routes/web.php
@@ -101,8 +101,8 @@ Route::get('/search','Goods\GoodsController@search');
 Route::get('/movie','Movie\IndexController@index');
 
 //微信
-Route::get('/weixin/test','Weixin\WeixinController@test');
-Route::get('/weixin/valid','Weixin\WeixinController@validToken');
-Route::get('/weixin/valid1','Weixin\WeixinController@validToken1');
-Route::post('/weixin/valid1','Weixin\WeixinController@wxEvent');        //接收微信服务器事件推送
-Route::post('/weixin/valid','Weixin\WeixinController@validToken');
+Route::get('/weixin/test','Weixin\IndexController@test');
+Route::get('/weixin/valid','Weixin\IndexController@validToken');
+Route::get('/weixin/valid1','Weixin\IndexController@validToken1');
+Route::post('/weixin/valid1','Weixin\IndexController@wxEvent');        //接收微信服务器事件推送
+Route::post('/weixin/valid','Weixin\IndexController@validToken');
