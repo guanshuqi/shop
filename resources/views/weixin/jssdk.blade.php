00729a21d4e317d604ca0125ca5d65d92bf9ef86
guanshuqi <17710293967@163.com>
2019-03-01 15:25:25 +0800

    weixin登录

--- resources/views/weixin/jssdk.blade.php
+++ resources/views/weixin/jssdk.blade.php
@@ -4,6 +4,7 @@
     <div class="container">
         <h1>JSSDK</h1>
         <button id="photo">选择图片</button>
+        <button id="sao">微信扫一扫</button>
     </div>
 @endsection
 @section('footer')
@@ -29,6 +30,16 @@
                     }
                 });
             })
+            $('#sao').click(function(){
+                wx.scanQRCode({
+                    needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+                    scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
+                    success: function (res) {
+                        var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
+                    }
+                });
+            })
+
         });
     </script>
 @endsection
