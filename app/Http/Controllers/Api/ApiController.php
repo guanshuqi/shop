5075197241f2fa26ee5486ed0478aa2161848c72
xiaoguan <xiaoguan1@localhost.localdomain>
2019-03-22 01:42:06 +0000

    token

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -68,28 +68,12 @@ class ApiController extends Controller
         $ch=curl_init();
         curl_setopt($ch,CURLOPT_URL,$url);
         curl_setopt($ch,CURLOPT_POST,true);//文件上传
-        curl_setopt($ch,CURLOPT_POSTFIELDS,$data); //文件上传
+        curl_setopt($ch,CURLOPT_POSTFIELDS,['data'=>$data]); //文件上传
         curl_setopt($ch,CURLOPT_HEADER,0);//不返回头部信息
         curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);//
         //抓取url传给浏览器
         $rs=curl_exec($ch);
         var_dump($rs);
-        $userInfo=UsersModel::where(['name'=>$name,'password'=>$pwd])->first();
-        $token = substr(md5(time().mt_rand(1,99999)),10,10);
-        $key='str:web:token:'.$userInfo->uid;
-        Redis::set($key,$token);
-        if($userInfo){
-            $response=[
-                'errno'=>200,
-                'msg'  =>'登录成功',
-                'token'=>$token
-            ];
-        }else{
-            $response=[
-                'errno'=>400,
-                'msg'  =>'账号或密码有误'
-            ];
-        }
     }
     //注册
     public function register(Request $request){
