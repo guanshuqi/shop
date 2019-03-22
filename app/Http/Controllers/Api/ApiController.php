a61f4811c84f1285908be0cc11a72356c96d6cc6
xiaoguan <xiaoguan1@localhost.localdomain>
2019-03-22 02:12:09 +0000

    token

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -68,7 +68,7 @@ class ApiController extends Controller
         $ch=curl_init();
         curl_setopt($ch,CURLOPT_URL,$url);
         curl_setopt($ch,CURLOPT_POST,true);//文件上传
-        curl_setopt($ch,CURLOPT_POSTFIELDS,['data'=>$data]); //文件上传
+        curl_setopt($ch,CURLOPT_POSTFIELDS,$data); //文件上传
         curl_setopt($ch,CURLOPT_HEADER,0);//不返回头部信息
         curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);//
         //抓取url传给浏览器
